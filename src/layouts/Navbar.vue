9c261fdc4539ffb4bb6b68a1250f09073a7141d4
unknown <cristiankukuh27@gmail.com>
2021-06-08 10:24:55 +0700

    add navbar menu icon

--- src/layouts/Navbar.vue
+++ src/layouts/Navbar.vue
@@ -1,8 +1,34 @@
 <template>
-  <div class="h-14 shadow-lg">
-    <div class="mx-3 h-full flex items-center">
-      <i class='bx bx-menu text-3xl' @click="minimizeSidebar"></i>
-      <!-- <h1>{{count}}</h1> -->
+  <div class="h-14 py-8 shadow-lg">
+    <div class="ml-3 mr-5 h-full flex items-center justify-between">
+      <!-- left menu -->
+      <div>
+        <i class='bx bx-menu text-3xl cursor-pointer' @click="minimizeSidebar"></i>
+      </div>
+      <!-- end left menu -->
+      <!-- right menu -->
+      <div class="flex items-center gap-7">
+        <div>
+          <i class='bx bx-search'></i>
+        </div>
+        <div>
+          <i class='bx bx-bell'></i>
+        </div>
+        <div>
+          <i class='bx bx-envelope'></i>
+        </div>
+        <div class="flex items-center gap-2">
+
+          <div class="rounded-full overflow-hidden shadow h-8 w-8">
+            <img src="img/4.jpg">
+          </div>
+          <div>
+            Jessica
+          </div>
+        </div>
+
+      </div>
+      <!-- end right menu -->
     </div>
   </div>
 </template>
